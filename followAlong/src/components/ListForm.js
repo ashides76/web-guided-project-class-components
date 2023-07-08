d02317280b6f6e224232586746b82fa5a08d1774
Ashish Desai <desai.ashish76@gmail.com>
2023-07-08 19:47:25 -0400

    guided class component project

--- followAlong/src/components/ListForm.js
+++ followAlong/src/components/ListForm.js
@@ -2,18 +2,33 @@ import React from "react";
 
 class ListForm extends React.Component {
   // Constructor with state
+  constructor(props){
+    super(props); 
+    this.state = {
+      item: '',
+    }
+  }
 
-  handleChanges = e => {
+
+
+  handleChanges = event => {
     // update state with each keystroke
+    event.preventDefault();
+    this.setState({...this.state, item: event.target.value})
   };
 
   // class property to submit form
+  submitForm = (event) => {
+    event.preventDefault();
+    this.props.addItem(event, this.state.item)
+    this.setState({...this.state, item:''})
+  }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitForm}>
         {/* This is an uncontrolled component 😬 We want it to be controlled by state */}
-        <input type="text" name="item" />
+        <input type="text" name="item" value={this.state.item} onChange={this.handleChanges}/>
         <button>Add</button>
       </form>
     );
--- followAlong/src/index.js
+++ followAlong/src/index.js
@@ -39,16 +39,57 @@ const groceries = [
 ];
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      groceries: groceries
+    }
+  }
+
+  // componentDidMount() {
+  //   console.log(this.state.groceries.map(item => item.purchased));
+  // }
+
   // Class methods to update state
+  addItem = (event, item) => {
+    event.preventDefault(); 
+    const newItem = {
+      name: item,
+      id: Date.now(),
+      purchased: false
+    }
+    this.setState({...this.state, groceries: [...this.state.groceries, newItem]})
+  }
+
+  toggleItem = (itemID) => {
+    console.log(itemID);
+    this.setState({...this.state, groceries: this.state.groceries.map(item => {
+      if (item.id === itemID) {
+        return {...this.state, purchased: !item.purchased}
+        this.setState(item.purchased = !item.purchased)
+      }
+      return item;
+    })})
+  }
+
+  clearPurchased = () => {
+    this.setState({...this.state, groceries: this.state.groceries.filter(item => {
+      if(!item.purchased) {
+        return item;
+      }
+    })})
+  }
+
   render() {
     return (
       <div className="App">
         <div className="header">
            <h1>Shopping List</h1>
-           <ListForm />
+           <ListForm addItem={this.addItem} />
          </div>
-        <GroceryList groceries={groceries} />
-        <button className="clear-btn">Clear Purchased</button>
+        <GroceryList groceries={this.state.groceries} toggleItem={this.toggleItem}/>
+        <button onClick={this.clearPurchased} className="clear-btn">Clear Purchased</button>
+        {/* <button onClick={(event) => this.addItem(event, 'orange')}>Add Orange</button> */}
        </div>
     );
   }
